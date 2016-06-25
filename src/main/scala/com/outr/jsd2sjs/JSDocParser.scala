3d01320e33f8824b278f0337c774cc303dd2dae8
Matt Hicks <matt@outr.com>
2016-06-24 22:19:30 -0500

    Fully tested for Canvas generation.

--- src/main/scala/com/outr/jsd2sjs/JSDocParser.scala
+++ src/main/scala/com/outr/jsd2sjs/JSDocParser.scala
@@ -6,6 +6,8 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import org.powerscala.io.IO
 
+import scala.collection.mutable.ListBuffer
+
 object JSDocParser {
   val baseUrl = "http://fabricjs.com"
 
@@ -22,55 +24,84 @@ object JSDocParser {
     val path = element.attr("href")
     val url = s"$baseUrl$path"
     val doc = Jsoup.connect(url).get()
+    val description = doc.select("body > div[id=main] > section > article > div[class=container-overview] > div[class=description]").text().trim
     val articles = doc.select("body > div[id=main] > section")
-    val matches = articles.select("article > h4[class=name], article > div[class=description]")
-    val items = (0 until matches.size()).map(matches.get(_).text().trim).toList.grouped(2).flatMap(objectInfo).toList
+    val matches = articles.select("article > h4[class=name], article > div[class=description], article > table[class=params] > tbody > tr > td[class=type]")
+    val iterator = (0 until matches.size()).map(matches.get(_).text().trim).toIterator
+    var items = ListBuffer.empty[ObjectInfo]
+    while (iterator.hasNext) {
+      items += objectInfo(iterator)
+    }
     val packageName = "com.outr.fabric"
     val completePackage = path.replace('/', '.').substring(6) match {
       case s => s.substring(0, s.length - 5)
     }
     val jsPackage = completePackage.substring(0, completePackage.lastIndexOf('.'))
     val name = completePackage.substring(completePackage.indexOf('.') + 1)
-    generate(packageName, jsPackage, name, items)
+    generate(packageName, jsPackage, name, description, items.toList)
   }
 
-  def objectInfo(items: List[String]): Option[ObjectInfo] = {
-    val heading = items.head
-    val colon = heading.indexOf(':')
-    if (colon == -1) {              // Method
-      val name = heading.substring(0, heading.indexOf('('))
-      val args = heading.substring(heading.indexOf('(') + 1, heading.indexOf(')')).split(",").map(_.trim).toList
-      val index = heading.indexOf('{')
-      val returnType = if (index > -1) {
-        val s = heading.substring(index + 1, heading.indexOf('}', index))
-        val lastDot = s.lastIndexOf('.')
-        if (lastDot > -1) {
-          s.substring(lastDot + 1)
-        } else {
-          s
+  def fixName(name: String): String = name match {
+    case "object" => "`object`"
+    case _ => name
+  }
+
+  def fixType(classType: String): String = classType match {
+    case s if s.startsWith("fabric.") => s.substring(7)
+    case "Event" => "org.scalajs.dom.Event"
+    case "Object" => "js.Object"
+    case "CanvasRenderingContext2D" => "org.scalajs.dom.CanvasRenderingContext2D"
+    case "HTMLCanvasElement" => "org.scalajs.dom.raw.HTMLCanvasElement"
+    case "HTMLElement | String" => "String"
+    case "Number | String" => "String"
+    case s => s
+  }
+
+  def objectInfo(iterator: Iterator[String]): ObjectInfo = {
+    val heading = iterator.next()
+
+    try {
+      val colon = heading.indexOf(':')
+      if (colon == -1) {
+        // Method
+        val name = heading.substring(0, heading.indexOf('('))
+        val args = heading.substring(heading.indexOf('(') + 1, heading.indexOf(')')).split(",").map(_.trim).toList.collect {
+          case s if s.nonEmpty => fixName(s)
         }
+        val index = heading.indexOf('{')
+        val returnType = if (index > -1) {
+          heading.substring(index + 1, heading.indexOf('}', index))
+        } else {
+          "Unit"
+        }
+        val description = iterator.next()
+        val argsWithTypes = args.map(argName => s"$argName: ${fixType(iterator.next())}")
+        MethodInfo(fixName(name), argsWithTypes, fixType(returnType), description)
       } else {
-        "Unit"
+        // Var
+        val name = heading.substring(0, colon).trim
+        val className = fixType(heading.substring(colon + 1).trim)
+        val description = iterator.next()
+        VarInfo(fixName(name), className, description)
       }
-      val description = items.tail.head.trim
-      Some(MethodInfo(name, args, returnType, description))
-    } else {                        // Var
-      val name = heading.substring(0, colon).trim
-      val className = heading.substring(colon + 1).trim
-      val description = items.tail.head.trim
-      Some(VarInfo(name, className, description))
+    } catch {
+      case t: Throwable => throw new RuntimeException(s"Failed to parse on $heading.", t)
     }
   }
 
-  def generate(packageName: String, jsPackage: String, name: String, entries: List[ObjectInfo]): String = {
+  def generate(packageName: String, jsPackage: String, name: String, description: String, entries: List[ObjectInfo]): String = {
     val b = new StringBuilder
     b.append(s"package $packageName\n\n")
     b.append("import scala.scalajs.js\n")
     b.append("import scala.scalajs.js.annotation.JSName\n\n")
+    b.append(s"/**\n")
+    b.append(s"  * $description\n")
+    b.append(s"  */\n")
     b.append("@js.native\n")
     b.append(s"""@JSName("$jsPackage.$name")\n""")
-    b.append(s"class $name extends js.Object {\n")
-    entries.foreach {
+    val initializer = entries.find(_.name == "initialize").get.asInstanceOf[MethodInfo]
+    b.append(s"class $name(${initializer.args.mkString(", ")}) extends js.Object {\n")
+    entries.filterNot(_.name == "initialize").foreach {
       case info: VarInfo => {
         b.append(s"  /**\n")
         b.append(s"    * ${info.description}\n")
@@ -89,7 +120,9 @@ object JSDocParser {
   }
 }
 
-trait ObjectInfo
+trait ObjectInfo {
+  def name: String
+}
 
 case class VarInfo(name: String, className: String, description: String) extends ObjectInfo
 
